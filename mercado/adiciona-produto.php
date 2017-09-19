770d3606c0dc556fc611fbd7f9127b004d51d53d
joanmelo89 <joanmelo89@gmail.com>
2017-09-18 21:58:29 -0300

    Att
    Att

--- mercado/adiciona-produto.php
+++ mercado/adiciona-produto.php
@@ -4,13 +4,17 @@
 	$preco = $_GET["preco"];
 	//cria a conexão com o banco
 	$conexao = mysqli_connect('localhost', 'root', '', 'mercado');
-	//criando a query de inserção
-	$query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+	function insereProdutos($conexao, $nome, $preco){
+		$query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+		return mysqli_query($conexao, $query);
+	}
+	
+	
 	//execução da query e verificando SE existe
-	if(mysqli_query($conexao, $query)){?>
-	<p class="alert-success">Produto <?php echo $nome;?>, <?php echo $preco;?> adicionado com sucesso!</p>
+	if(insereProdutos($conexao, $nome, $preco)){?>
+	<p class="text-success">Produto <?php echo $nome;?>, <?php echo $preco;?> adicionado com sucesso!</p>
 	<?php } else{ ?>
-	<p class="alert-danger">Produto não foi adicionado!</p>
+	<p class="text-danger">Produto não foi adicionado!</p>
 	<?php	
 	}
 	//fechamento da conexão
--- mercado/produto-formulario.php
+++ mercado/produto-formulario.php
@@ -1,13 +1,20 @@
 <?php include("cabecalho.php"); ?>
 			<h1>Formulário de Produtos</h1>
+			<table class="table">
 			<form action="adiciona-produto.php">
-			Nome:
-			<input type="text" name="nome"><br>
-			Preço:
-			<input type="number" name="preco"><br>
-
-			<input type="submit"  value="Enviar">
+			<tr>
+			<td>Nome:</td>
+			<td><input class="form-control" type="text" name="nome"></td>
+			</tr>
+			<tr>
+			<td>Preço:</td>
+			<td><input class="form-control" type="number" name="preco"></td>
+			</tr>
+			<tr>
+			<td><input class="btn btn-primary" type="submit"  value="Enviar"></td>
+			</tr>
 
 
 			</form>
+			</table>
 <?php include("rodape.php"); ?>
