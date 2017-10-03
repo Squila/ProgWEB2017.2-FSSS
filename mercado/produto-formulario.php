58bcc5ede0c2db0cf3158338c16a4ddbf9cee5d8
joanmelo89 <joanmelo89@gmail.com>
2017-10-03 20:36:20 -0300

    att
    att

--- mercado/produto-formulario.php
+++ mercado/produto-formulario.php
@@ -15,6 +15,17 @@
 			<td><textarea name="descricao" class="form-control"></textarea></td>
 			</tr>
 			<tr>
+			<td>Categoria:</td>
+			<td>
+				<select class="form-control">
+					<option value="1">esporte</option>
+					<option value="2">mobilidade</option>
+					<option value="3">escolar</option>
+				</select>
+			</td>
+
+			</tr>
+			<tr>
 			<td><input class="btn btn-primary" type="submit"  value="Enviar"></td>
 			</tr>
 
--- mercado/produto-lista.php
+++ mercado/produto-lista.php
@@ -1,11 +1,13 @@
 <?php include("cabecalho.php"); ?>
 <?php include("conecta.php"); ?>
 <?php include("banco-produto.php"); ?>
+<?php include("banco-categoria.php"); ?>
 <?php
 
 
 
 $produtos = listaProdutos($conexao);
+$categorias = listaCategorias($conexao);
 
 ?>
 <table class="table table-striped table-bordered">
@@ -13,6 +15,7 @@ $produtos = listaProdutos($conexao);
 		<td><b>Nome</b></td>
 		<td><b>Descrição</b></td>
 		<td><b>Preço</b></td>
+		<td><b>Categoria</b></td>
 	</tr>
 <?php
 
@@ -22,6 +25,7 @@ $produtos = listaProdutos($conexao);
 <td><?php echo $produto["nome"];?></td>
 <td><?php echo substr($produto["descricao"], 0, 15);?></td>
 <td><?php echo $produto["preco"];?></td>
+<td><?php echo $produto["categoria_id"];?></td>
 <td>
 	<form action="remove-produto.php" method="post">
 	<input type="hidden" name="id" value="<?php echo $produto["id"];?>">
